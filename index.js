c5d70f1da30093388d5579029635c5ac60123b7c
Aden Addis <adenwonde56@gmail.com>
2022-06-29 19:37:24 -0400

    commit tree c7c84040f6421dd8a5daa4929470e021ea9ce638

---
+++ index.js
@@ -0,0 +1,9 @@
+
+// const heading = document.querySelector("h1");
+
+// console.log(heading);
+// heading.innerText = "Flatiron School is ther best school!"
+
+// const paragraph = document.getElementById("paragraph")
+// console.log(paragraph)
+// // paragraph.remove()
